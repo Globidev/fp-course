27aa89936d4a3d2ae4bb4016e4507c56fba04121
Guillaume Depardon <guillaume.depardon@gmail.com>
2018-08-04 19:14:55 +0200

    Comonad

--- src/Course/Comonad.hs
+++ src/Course/Comonad.hs
@@ -29,8 +29,8 @@ instance Comonad ExactlyOne where
   copure ::
     ExactlyOne a
     -> a
-  copure =
-    error "todo: Course.Comonad copure#instance ExactlyOne"
+  copure (ExactlyOne a) =
+    a
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
@@ -42,4 +42,4 @@ instance Comonad ExactlyOne where
   -> f a
   -> f b
 (<$$>) =
-  error "todo: Course.Comonad#(<$>)"
+  (<<=) . (. copure)
